43e8dd5db953006a086c2a2e9736ffc18a9d2ea1
amyap306 <amporter@claflin.edu>
2026-08-18 16:14:02 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,27 @@
+#Name: A'Mya Porter, Date: 08/18/2026, Purpose: To test functions of descriptive stats.
+
+#Create a variable called variable1 and store 5 numbers from 1-5
+
+variable1 <- c(1,2,3,4,5)
+> variable1
+[1] 1 2 3 4 5
+
+#Calculate the mean/average of variable1
+
+> mean(variable1)
+[1] 3
+
+#Calculate standard deviation or variable1
+
+> sd(variable1)
+[1] 1.581139
+
+#Calculate the median of variable1
+
+> median(variable1)
+[1] 3
+
+#Calculate the mode of variable1
+
+> mode(variable1) (dont need that buddy)
+[1] "numeric"
